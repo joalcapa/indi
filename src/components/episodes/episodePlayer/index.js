32083f5d90cc234f9db3b1d65e648944286a724e
Jose Caceres <josecaceres.oreul@gmail.com>
2023-07-02 23:27:58 -0500

    feat: Add description html

--- src/components/episodes/episodePlayer/index.js
+++ src/components/episodes/episodePlayer/index.js
@@ -1,33 +1,31 @@
-import { useRef, useEffect } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import './index.css';
 
 const EpisodePlayer = (props) => {
-    console.log('POPRS..:', props);
-
     const controlId = "use-uuid-if-multiple-on-page";
 
     useEffect(() => {
         const audioControl = document.querySelector(`#${controlId}`);
         if(audioControl) {
-            //Register the changed source
             audioControl.load();
         }
-    })
-    
+    });
+
     return (
-    <div className='episodes-list-content box-shadow'>
-        <div className='episode-title'>{props.episode.title}</div>
-        <div className='episode-description'>{props.episode.description}</div>
-        <div className='episode-player'>
-        <audio id={controlId} controls preload="auto" className='player'>
-        <source src={props.episode.raw.url} type={props.episode.raw.type}/>
-    </audio>
-      
+        <div className='episodes-list-content box-shadow'>
+            <div className='episode-title'>{props.episode.title}</div>
+            <div className='episode-description'>
+                <div dangerouslySetInnerHTML={{ __html: props.episode.description }}></div>
+            </div>
+            <div className='episode-player'>
+                <audio id={controlId} controls preload="auto" className='player'>
+                    <source src={props.episode.raw.url} type={props.episode.raw.type}/>
+                </audio>
+            </div>
         </div>
-    </div>
-);
-    };
+    );
+};
 
 EpisodePlayer.propTypes = {
     episode: PropTypes.shape({
